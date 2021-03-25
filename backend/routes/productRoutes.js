e5d115d6be5fb8411f7843ddf6184582923ed4a3
robdan89 <fuquardwork@gmail.com>
2021-03-25 13:34:51 -0500

    added backend for create and update products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,15 +3,18 @@ import {
   getProductById,
   getProducts,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from "../controllers/productController.js";
 import { isAdmin, protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, isAdmin, createProduct);
 router
   .route("/:id")
   .get(getProductById)
-  .delete(protect, isAdmin, deleteProduct);
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct);
 
 export default router;
